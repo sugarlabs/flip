f9eeb5fe5c72d476ac742eabbc5885c97457e8aa
sourabhaa <sourabhayoga@gmail.com>
2021-08-29 10:41:07 +0530

    fixed typo

--- game.py
+++ game.py
@@ -416,14 +416,14 @@ class Game():
 
     def read_best_time(self):
         best_time = [180, 180, 180, 180]
-        file_path = os.path.join(get_activity_root(), 'data', 'Best-tim')
+        file_path = os.path.join(get_activity_root(), 'data', 'best-time')
         if os.path.exists(file_path):
             with open(file_path, "r") as fp:
                 best_time = fp.readline()
         return best_time
 
     def save_best_time(self):
-        file_path = os.path.join(get_activity_root(), 'data', 'Best-tim')
+        file_path = os.path.join(get_activity_root(), 'data', 'best-time')
         best_time = self.read_best_time()
         if isinstance(best_time, str):
             best_time = self.convert_to_int_list(best_time)
